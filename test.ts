67837d94afb4486ab644a91cf3414c361b67d81d
zhang-tang <80501060+zhang-tang@users.noreply.github.com>
2023-06-12 17:20:17 +0800

    更新 main.blocks, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,17 +1,17 @@
-serial.writeLine("===================")
-voiceRecognition.init()
-voiceRecognition.setVolume(4)
-voiceRecognition.setMuteMode(voiceRecognition.MUTE.OFF)
-voiceRecognition.setWakeTime(20)
-serial.writeLine("" + (voiceRecognition.getWakeTime()))
-voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
-serial.writeLine("=================")
-basic.forever(function () {
-    voiceRecognition.getCMDID()
-    if (voiceRecognition.checkCMDID()) {
-        serial.writeLine("" + (voiceRecognition.readCMDID()))
-        if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W22)) {
-            serial.writeLine("2222222222")
-        }
-    }
-})
+// serial.writeLine("===================")
+// voiceRecognition.init()
+// voiceRecognition.setVolume(4)
+// voiceRecognition.setMuteMode(voiceRecognition.MUTE.OFF)
+// voiceRecognition.setWakeTime(20)
+// serial.writeLine("" + (voiceRecognition.getWakeTime()))
+// voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
+// serial.writeLine("=================")
+// basic.forever(function () {
+//     voiceRecognition.getCMDID()
+//     if (voiceRecognition.checkCMDID()) {
+//         serial.writeLine("" + (voiceRecognition.readCMDID()))
+//         if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W22)) {
+//             serial.writeLine("2222222222")
+//         }
+//     }
+// })
